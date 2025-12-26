192852309fc1bb8966614510c8cae6f5349fe24d
ChauKhanhLy <ckhanhly2209@gmail.com>
2025-12-26 23:45:23 +0700

    update backend feedbackPage

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,5 +1,6 @@
 import { createOrder } from "../dal/orders.dal.js"
 import { createOrderDetail } from "../dal/orderDetail.dal.js"
+import { supabase } from "../database/supabase.js"
 
 export const checkout = async (req, res) => {
   try {
@@ -30,3 +31,32 @@ export const checkout = async (req, res) => {
     res.status(500).json({ error: err.message })
   }
 }
+
+export const getRecentOrders = async (req, res) => {
+  const { user_id } = req.query;
+
+  const { data, error } = await supabase
+    .from("orders")
+    .select(`
+      id,
+      created_at,
+      orderDetails (
+        food ( name )
+      )
+    `)
+    .eq("user_id", user_id)
+    .order("created_at", { ascending: false })
+    .limit(3);
+
+  if (error) return res.status(500).json({ error: error.message });
+
+  const result = data.map(order => ({
+    id: order.id,
+    orderId: `#${order.id.slice(0, 6)}`,
+    time: new Date(order.created_at).toLocaleString(),
+    items: order.orderDetails.map(d => d.food.name)
+  }));
+
+  res.json(result);
+};
+
