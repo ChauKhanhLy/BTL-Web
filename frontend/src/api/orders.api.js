89514c2363d2fb47fb7b6d683efcbcb66b6594e8
manh2k10 <161539771+manh2k10@users.noreply.github.com>
2025-12-27 11:02:00 +0700

    Update orders.api.js

--- frontend/src/api/orders.api.js
+++ frontend/src/api/orders.api.js
@@ -4,3 +4,7 @@ const API_URL = "http://localhost:5000/api/orders";
 
 export const getRecentOrders = (user_id) =>
   axios.get(API_URL, { params: { user_id } });
+
+export const getOrderStats = (range) => {
+  return axiosClient.get(`/orders/stats?range=${range}`);
+};
